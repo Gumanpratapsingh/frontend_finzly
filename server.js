37964b36235b985eff9c8ebd1c904432369c0f32
Gumanpratapsingh <gumanpratapsinghparmar@gmail.com>
2024-10-02 23:25:37 +0530

    first commit

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve static files from the dist directory
+app.use(express.static(path.join(__dirname, 'dist/finzly-gps-frontend')));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// Send index.html for any other requests
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/finzly-gps-frontend/index.html'));
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
